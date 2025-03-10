2c90e6ab7f148e7d3392f07e5d82d4e928a0a1e2
Jaro(mir) J <jaromir.j@icloud.com>
2025-03-10 10:51:18 +0100

    Added filtering by language and changed a bit logic related to search parameters handeling

---
+++ Books-repository/Engine/BooksSearchParameters.swift
@@ -0,0 +1,8 @@
+//
+//  BooksSearchParameters.swift
+//  Books-repository
+//
+//  Created by Jaromir Jagieluk on 08/03/2025.
+//
+
+import Foundation
---
+++ Books-repository/Views/LanguageFilterView.swift
@@ -0,0 +1,18 @@
+//
+//  LanguageFilterView.swift
+//  Books-repository
+//
+//  Created by Jaromir Jagieluk on 10/03/2025.
+//
+
+import SwiftUI
+
+struct LanguageFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LanguageFilterView()
+}
